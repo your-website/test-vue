bd43d8657b8e3bfe97aaf4bb75a19220b40d4a6a
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-01-26 20:43:44 +0700

    added gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/test-vue/'
+    : '/',
   pluginOptions: {
     'style-resources-loader': {
       'preProcessor': 'scss',
